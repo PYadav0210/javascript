8dc3e307d73e446e98db75998d3b5c8b9c980424
PYadav0210 <78567490+PYadav0210@users.noreply.github.com>
2024-07-19 20:03:32 +0000

    conversion of datatypes

---
+++ basics/test.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountEmail = "ypurshotam50@gmail.com"
+var accountPassword = 123455
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword]);
+
+let score = "Fuck off";
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);

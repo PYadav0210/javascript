7f32b9c9485460930f93191cfe7bb735c90ef3f7
PYadav0210 <78567490+PYadav0210@users.noreply.github.com>
2024-10-05 23:48:21 +0000

    Ojects done

--- basics/test.js
+++ basics/test.js
@@ -10,3 +10,28 @@ console.log(typeof(score));
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+
+// Object literals
+const mysym = Symbol("hello1")
+const jsuser = {
+    [mysym] : "mykeyvalue",
+  name : "Dotted",
+  age : 16,
+  email: "ypurshotam50@gmail.com"
+
+}
+
+jsuser.greeting = function(){
+    console.log("Hello Greeting")
+}
+jsuser.greetingTwo = function(){
+    console.log(`Hello Greeting, ${this.email}`)
+}
+console.log(jsuser["email"])
+console.log(jsuser["age"])
+console.log(typeof jsuser[mysym])
+console.log(jsuser.greeting())
+console.log(jsuser.greetingTwo())
+
+
